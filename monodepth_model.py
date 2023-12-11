de33ff39a6e74659c25dc59d606d7965eb7fbbcb
SongYewon <88762541+Yewon-Song@users.noreply.github.com>
2023-12-11 15:05:20 +0900

    Update monodepth_model.py

--- monodepth_model.py
+++ monodepth_model.py
@@ -187,31 +187,25 @@ class MonodepthModel(object):
         
         with tf.variable_scope('decoder'):
             upconv7 = upconv(conv7,  512, 3, 2) #H/64
-            concat7 = tf.concat([upconv7, skip6], 3)
             iconv7  = conv(concat7,  512, 3, 1)
 
             upconv6 = upconv(iconv7, 512, 3, 2) #H/32
-            concat6 = tf.concat([upconv6, skip5], 3)
             iconv6  = conv(concat6,  512, 3, 1)
 
             upconv5 = upconv(iconv6, 256, 3, 2) #H/16
-            concat5 = tf.concat([upconv5, skip4], 3)
             iconv5  = conv(concat5,  256, 3, 1)
 
             upconv4 = upconv(iconv5, 128, 3, 2) #H/8
-            concat4 = tf.concat([upconv4, skip3], 3)
             iconv4  = conv(concat4,  128, 3, 1)
             self.disp4 = self.get_disp(iconv4)
             udisp4  = self.upsample_nn(self.disp4, 2)
 
             upconv3 = upconv(iconv4,  64, 3, 2) #H/4
-            concat3 = tf.concat([upconv3, skip2, udisp4], 3)
             iconv3  = conv(concat3,   64, 3, 1)
             self.disp3 = self.get_disp(iconv3)
             udisp3  = self.upsample_nn(self.disp3, 2)
 
             upconv2 = upconv(iconv3,  32, 3, 2) #H/2
-            concat2 = tf.concat([upconv2, skip1, udisp3], 3)
             iconv2  = conv(concat2,   32, 3, 1)
             self.disp2 = self.get_disp(iconv2)
             udisp2  = self.upsample_nn(self.disp2, 2)
